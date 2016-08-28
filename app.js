973c34551778bdfac1af9fd0ebfe8748976b597f
Eagle Wu <eaglewu014@gmail.com>
2016-08-28 14:38:48 -0400

    BREAK HEROKU WALLLLL111111!!!!!!

--- app.js
+++ app.js
@@ -21,7 +21,19 @@ var profile   = require('./routes/profile');
 var api       = require('./routes/api');
 
 var app = express();
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,6 +44,7 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(allowCrossDomain);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
