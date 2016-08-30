9dd5ff5cee4484c091dbd83fbe17373c2b56e142
Eagle Wu <eaglewu014@gmail.com>
2016-08-30 13:10:43 -0400

    added new pages for index

--- routes/profile.js
+++ routes/profile.js
@@ -10,10 +10,10 @@ router.get('/do_index', function(req, res, next){
 	res.render('do')
 });
 router.get('/community_index', function(req, res, next){
-	res.render('community')
+	res.render('TheCommunity')
 })
 router.get('/projects_index', function(req, res, next){
-	res.render('projects_index')
+	res.render('Projects')
 })
 router.use(function(req, res, next){
     	if(!req.user){
@@ -62,11 +62,29 @@ router.post('/profileInfo', function(req, res, next){
 	})
 })
 
+router.put('/profileInfoNotes', function(req, res, next){
+	var update = {
+		notes: req.body.notes
+	}
+
+	User.findByIdAndUpdate(req.user.id, update, function(erro, user){
+		error ? res.json({"success": false, "message": error}) : null
+		User.findById(req.user.id, function(err, user){
+			error ? res.json({"success": false, "message" : err}) : req.json({"success": true, "user": user})
+		})
+	})
+})
+
 router.put('/profileInfo', function(req, res, next){
 	var update = {
 		motto: req.body.motto,
+		name: req.body.name,
 		bio: req.body.bio, 
-		skills: req.body.skills
+		skills: req.body.skills.toString().split(","),
+		notes: req.body.notes,
+		email: req.body.email,
+		phoneNumber: req.body.phone,
+		website: req.body.website
 	}
 
 	User.findByIdAndUpdate(req.user.id, update, function(error, user){
