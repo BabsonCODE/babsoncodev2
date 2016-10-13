dc71de2200ced653e6b43b60eeb8a543e9d4fb3a
EagleWuDev <eaglewu014@gmail.com>
2016-10-13 02:30:39 -0400

    finished projects and posts

--- public/javascripts/posts.js
+++ public/javascripts/posts.js
@@ -1,21 +1,49 @@
 var clearBoard = function(){
-
+	$('#postingBoard').html('');
 }
 
 
-var render = function(){
+var render = function(posts){
+	console.log(posts);
+	console.log("render being called");
 
+	posts.forEach(function(post, index){
+		var wrapper = $('<div></div>');
+        var member = $('<div class = "member"></div>');
+        var image = $('<div class = "memberImage" style = "background-image:url(' + post.user.imageUrl + ')"></div>');
+        var info = $('<div class= "memberInfo"></div>');
+        var name = $('<h3><a href="/users/' +  post.user._id + '">' + post.user.name + '</a></h3>');
+        var content = $('<p><span class="bold">' + post.content + '</span></p>')
+        var time = $('<p>' + post.createdAt + '</p>');
+
+        wrapper.append(member);
+        member.append(image);
+        member.append(info);
+        info.append(name);
+        info.append(content);
+        info.append(time);
+
+        wrapper.html();
+
+        $('#postingBoard').append(wrapper);
+
+	})
 }
 
 var getPosts = function(projectId){
 	$.get("/posts/" + projectId,
 		function(data, status){
 			console.log(data);
+			var posts = data.posts;
+			console.log(posts);
+			console.log('getPosts being called')
+			render(posts);
 		})
 }
 
-var mountBoard = function(){
-
+var mountBoard = function(projectId){
+	clearBoard();
+	getPosts(projectId);
 }
 
 
@@ -26,6 +54,7 @@ var setPosts = function(postContent, projectId){
 			content: postContent,
 			projectId: projectId
 		}, function(data, status){
-			console.log(data, status)
+			console.log(data, status);
+			mountBoard(projectId);
 		})
 }
